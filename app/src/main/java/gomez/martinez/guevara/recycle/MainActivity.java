c967a34b5c7b51a7869338dd23e063446cd1dacc
Julio-Guevara <35203056+Julio-Guevara@users.noreply.github.com>
2018-05-15 21:16:31 -0500

    otra imagen y ajustar el recyclerview

--- app/src/main/java/gomez/martinez/guevara/recycle/MainActivity.java
+++ app/src/main/java/gomez/martinez/guevara/recycle/MainActivity.java
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Lugar> lugares = new ArrayList<>();
 
         lugares.add(new Lugar("https://www.thenational.ae/image/policy:1.664553:1507218840/Wk06-Mel5.jpg?f=16x9&w=1200&$p$f$w=204e5d9","Melbourne"));
-        lugares.add(new Lugar("https://img.ev.mu/images/villes/5580/1605x642/5580.jpg","paris"));
-        lugares.add(new Lugar("https://imvid.depor.com/files/article_multimedia/uploads/2017/01/04/586d84f0dcf15.jpeg","rio de janeiro"));
+        lugares.add(new Lugar("https://img.ev.mu/images/villes/5580/1605x642/5580.jpg","París"));
+        lugares.add(new Lugar("https://imvid.depor.com/files/article_multimedia/uploads/2017/01/04/586d84f0dcf15.jpeg","Rio de Janeiro"));
+        lugares.add(new Lugar("http://foodandtravel.mx/home/wp-content/uploads/2017/01/shutterstock_417642688-e1484160058190.jpg","Hamburgo"));
 
         rv.setLayoutManager(new GridLayoutManager(getApplicationContext(),1));
         rv.setHasFixedSize(true);
